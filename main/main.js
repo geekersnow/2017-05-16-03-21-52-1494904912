95de0b4b850e2902c09349b11933159a113649b7
geekersnow <geekersnow@users.noreply.github.com>
2017-06-07 20:42:26 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,6 +1,6 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
-  var str = email.spilt('@');
+  var str = email.spilt("@");
   for (var i = 0; i< length.suffixes; i++)
 	  if (str[1] == suffixes[i])
 		return true;
