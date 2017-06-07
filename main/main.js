23d5e61b36290b590e82f5b3ac2ae5fcc6428812
geekersnow <geekersnow@users.noreply.github.com>
2017-06-07 20:43:42 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,6 +1,6 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
-  var str = email.spilt("@");
+  var str = email.split("@");
   for (var i = 0; i< length.suffixes; i++)
 	  if (str[1] == suffixes[i])
 		return true;
